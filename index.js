86e89e980ba46be81b7c78d9b6abf66885ced973
Danny Rivera <samboard@hotmail.com>
2021-05-06 14:48:42 -0400

    commit tree b196b22a4e8b79f3ad45475406a802282979fee3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 document.querySelector("button").addEventListener("click", handleClick);
 
-function handleClick(e) {
+function handleClick() {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
@@ -10,4 +10,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr) {
+  const hour = parseInt(timeStr);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
